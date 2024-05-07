3b0f72aa20efab5a63953ebcb1a5040fef88ad24
MagnarIUK <magnariuk@gmail.com>
2024-05-07 17:47:35 +0300

    Add project files.

---
+++ LemonkaTools/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LemonkaTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class DataContextModel
+        {
+            public string ProgramVersion { get; } = "A-1.0.0";
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DataContextModel();
+            if (File.Exists(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json")))
+            {
+                var config = Tools.Read<Dictionary<string, Dictionary<string, List<string>>>>("config.json");
+
+
+
+
+
+            }
+
+        }
+    }
+}
---
+++ LemonkaTools/Tools.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace LemonkaTools
+{
+    internal class Tools
+    {
+        public static async Task Write<T>(string file_name, T data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file_name), false))
+            {
+                await writer.WriteAsync(json);
+            }
+        }
+
+        private static void  OpenFolder(string Path)
+        {
+            if (Directory.Exists(Path))
+            {
+                Process.Start("explorer.exe", Path);
+            }
+            else
+            {
+                MessageBox.Show("Директорія не існує!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private static void OpenLink(string url)
+        {
+            Process.Start(new ProcessStartInfo(url));
+        }
+
+        public static List<string> GetFilesInFolder(string folderPath)
+        {
+            List<string> files = new List<string>();
+
+            try
+            {
+                string[] filePaths = Directory.GetFiles(folderPath);
+                foreach (string filePath in filePaths)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    files.Add(fileName);
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                files.Add("Файлів нема");
+            }
+
+            return files;
+        }
+
+        public static void ClearFile(string fileName)
+        {
+            try
+            {
+                using (var stream = new FileStream(fileName, FileMode.OpenOrCreate))
+                {
+                    stream.SetLength(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+
+        public static T Read<T>(string fileName)
+        {
+            try
+            {
+                var extension = Path.GetExtension(fileName);
+
+                if (extension == ".json")
+                {
+                    var json = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName));
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+                else if (extension == ".txt")
+                {
+                    var text = File.ReadAllText(fileName);
+                    return (T)(object)text;
+                }
+                else
+                {
+                    throw new ArgumentException("Непідтримуваний формат файлу");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Виникла помилка: {ex.Message}");
+                return default;
+            }
+        }
+
+        public static string AskDirectory()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                return dialog.FileName;
+            }
+            return null;
+
+        }
+
+        public static string AskFile(string filter)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = filter;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                return selectedFileName;
+            }
+            return null;
+
+        }
+
+    }
+}
